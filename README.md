ab9a05ee7b9d418595319aa8ab246fea299d68b7
cooperhancock <23041745+dakotahlake@users.noreply.github.com>
2021-10-18 11:55:59 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # youtube_downloader
+
+run `pip install pytube` to use
---
+++ youtube_downloader.py
@@ -0,0 +1,28 @@
+# importing the module 
+from pytube import YouTube 
+import os
+
+# where to save - default downloads folder
+SAVE_PATH = os.path.expanduser("~")+"\Downloads\\" 
+
+# check save path
+print(SAVE_PATH)
+check = input("Use save path? y or enter new path: ")
+if(check != 'y'):
+    SAVE_PATH = check
+
+# link of the video to be downloaded 
+link=input("Enter video link: ")
+
+try: 
+	# object creation using YouTube 
+	# which was imported in the beginning 
+	yt = YouTube(link) 
+except: 
+	print("Connection Error") #to handle exception 
+stream = yt.streams.filter(progressive=True).first()
+print(stream)
+stream.download(SAVE_PATH)
+
+ 
+
